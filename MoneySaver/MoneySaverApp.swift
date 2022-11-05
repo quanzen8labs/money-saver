3bf2157687580ac2627019e51f34a84191d775e1
quanzen8labs <quan@zen8labs.com>
2022-11-05 14:07:41 +0700

    Init project

---
+++ MoneySaver/MoneySaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneySaverApp.swift
+//  MoneySaver
+//
+//  Created by macbook on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneySaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Money Saver
+## _Save your money day by day_
+
+I lose my money a lot.
+One a day, I make this app for my self with purposes:
+- Save my money
+- Learn SwiftUI and Composable Architecture
+
+## Features
+- Save bill day by day
